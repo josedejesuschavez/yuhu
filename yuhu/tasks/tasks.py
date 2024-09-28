634c3d8776b100dd8d6e81f4a08ef23db6eddd4e
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2024-09-28 07:16:42 -0600

    --

--- yuhu/tasks/tasks.py
+++ yuhu/tasks/tasks.py
@@ -4,13 +4,14 @@ from time import sleep
 from django.core.mail import send_mail
 
 from tasks.domain.events.task_created_event import TaskCreatedEvent
+from yuhu.settings import EMAIL_HOST_USER
 
 
 @shared_task
 def send_task_created_email(event):
     subject = f'Task Created: {event['title']}'
     message = f'A new task has been created with ID: {event['task_id']}, title: {event['title']} and description: {event['description']}.'
-    from_email = 'testjosedejesustest@gmail.com'
+    from_email = EMAIL_HOST_USER
 
     send_mail(subject, message, from_email, [event['email']])
 
@@ -18,6 +19,6 @@ def send_task_created_email(event):
 def send_task_updated_email(event):
     subject = f'Task Updated: {event['title']}'
     message = f'A new task has been updated with ID: {event['task_id']}, title: {event['title']} and description: {event['description']}.'
-    from_email = 'testjosedejesustest@gmail.com'
+    from_email = EMAIL_HOST_USER
 
     send_mail(subject, message, from_email, [event['email']])
